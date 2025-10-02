2b9bc5d281a74698b18dca8649bfd29e933e75ae
Wael Bouhaya <waelbouhaya@gmail.com>
2025-10-03 00:01:08 +0100

    Update poo_act19_qst_7.py

--- poo_act19_qst_7.py
+++ poo_act19_qst_7.py
@@ -1,15 +1,13 @@
 # Act 19 :
 # Qst 7 :
 def F7(n):
-    if n == 0 :
-        return 0
-    elif n == 1 :
-        return 1
-    else :
-        return F7(n-1)+F7(n-2)
+    if n == 0 or n==1 :
+        return n
+    return F7(n-1)+F7(n-2)
     
 n = int(input("Entrer le n-eme terme : "))
 print(F7(n))
 
 # Nom Complet : WAEL BOUHAYA
+
 # Filiere : IAGI-1 
